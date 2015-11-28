8a31c6ea5741c2e0539faf16af6fa67c445751bc
“Daliso <daliso.zuze@gmail.com>
2015-11-28 16:11:33 +0000

    Testing dropping of pins based on locations

--- Where2Go/ExplorePlacesViewController.swift
+++ Where2Go/ExplorePlacesViewController.swift
@@ -11,26 +11,60 @@ import MapKit
 
 class ExplorePlacesViewController: UIViewController {
     
-    // making a small change to initiatilse GIT
     @IBOutlet weak var exploreMapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Lets see if I can connect to Foursquare here and print out some location objects...
+        let demoLocations = MKW2GLocation.MKW2GLocationsFromW2GLocations(
+            W2GLocation.W2GLocationsFromResults(
+                createTestAPIResults()
+            )
+        )
         
-        // instead I will see if I can create some dummy location objects and display them on the map...
         
-        // so what does a location object need to look like
-        
-        
-        
+        exploreMapView.addAnnotations(demoLocations)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func createTestAPIResults() -> [[String: AnyObject]] {
+        
+        var testResults = [[String: AnyObject]]()
+        
+        testResults.append(
+            [
+                FoursquareAPIClient.JSONResponseKeys.venueID : "4b23b0a5f964a520175824e3",
+                FoursquareAPIClient.JSONResponseKeys.venueName : "YO! Sushi",
+                FoursquareAPIClient.JSONResponseKeys.Latitude : 51.50692612,
+                FoursquareAPIClient.JSONResponseKeys.Longitude : -0.22103548
+            ]
+        )
+        
+        testResults.append(
+            [
+                FoursquareAPIClient.JSONResponseKeys.venueID : "5172f248e4b09adb685ea45f",
+                FoursquareAPIClient.JSONResponseKeys.venueName : "Mr Sushi",
+                FoursquareAPIClient.JSONResponseKeys.Latitude : 51.50312037114186,
+                FoursquareAPIClient.JSONResponseKeys.Longitude : -0.2237503036553875
+            ]
+        )
+        
+        testResults.append(
+            [
+                FoursquareAPIClient.JSONResponseKeys.venueID : "55412714498eba342dfd063f",
+                FoursquareAPIClient.JSONResponseKeys.venueName : "Sticks N Sushi",
+                FoursquareAPIClient.JSONResponseKeys.Latitude : 51.50603657743944,
+                FoursquareAPIClient.JSONResponseKeys.Longitude : -0.018242708075009856
+            ]
+        )
+        
+        return testResults
     }
 
 
