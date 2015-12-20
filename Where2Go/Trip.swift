e284b813d4e5d560b5f08160cb9be409ab840975
“Daliso <daliso.zuze@gmail.com>
2015-12-20 17:44:33 +0000

    Added the Trip managed objects

---
+++ Where2Go/Trip.swift
@@ -0,0 +1,17 @@
+//
+//  Trip.swift
+//  Where2Go
+//
+//  Created by Daliso Zuze on 20/12/2015.
+//  Copyright © 2015 Daliso Zuze. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Trip: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
